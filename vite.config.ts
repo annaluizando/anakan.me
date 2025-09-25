075e16d704876c02faf10d0e1d02b60a03e39436
annaluizando <annaluiza.ndo@gmail.com>
2025-09-25 19:28:51 -0300

    fix: correcting site build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [tailwindcss(), react()],
   assetsInclude: ["**/*.md"],
 });
