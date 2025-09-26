d716fd8621f83cde2cc7c8a8afdd5afb36c6c94b
annaluizando <annaluiza.ndo@gmail.com>
2025-09-26 10:57:47 -0300

    fix: devascan ascii

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -28,8 +28,13 @@ export const projects: ProjectProps[] = [
   {
     id: "devascan",
     title: `
- ██▄ ██▀ █ █ ▄▀█ ▄▀▀ ▄▀▀ ▄▀█ █▄ █
- ██▄ ██▄ ▀▄▀ █▄█ ▄▄▀ ▀▄▄ █▄█ █ ▀█`,
+    .___                                               
+  __| _/_______  _______    ______ ____ _____    ____  
+ / __ |/ __ \\  \\/ /\\__  \\  /  ___// ___\\\\__  \\  /    \\ 
+/ /_/ \\  ___/\\   /  / __ \\_\\___ \\\\  \\___ / __ \\|   |  \\
+\\____ |\\___  >\\_/  (____  /____  >\\___  >____  /___|  /
+     \\/    \\/           \\/     \\/     \\/     \\/     \\/ 
+     `,
     description:
       "Project infrastructure scanner to identify best approaches to improve security pipeline.",
     githubUrl: "https://github.com/annaluizando/devascan",
